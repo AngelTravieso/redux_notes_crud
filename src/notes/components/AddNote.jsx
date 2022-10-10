315f30bd006080f1df4910077e9a1af10aaeb5c8
Angel Travieso <angeltraviesoc@gmail.com>
2022-10-10 13:44:57 -0400

    validacion formulario nota

--- src/notes/components/AddNote.jsx
+++ src/notes/components/AddNote.jsx
@@ -1,17 +1,40 @@
 import { Box, Button, Grid, TextField, Typography } from "@mui/material"
+import { useState } from "react"
+import { useForm } from "../../hooks/useForm"
+
+const formData = {
+    note: '',
+}
+
+const formValidation = {
+    note: [ (value) => value.length >= 3, 'La nota debe contener mínimo 3 letras.'],
+}
 
 export const AddNote = () => {
 
+    const [formSubmitted, setFormSubmitted] = useState(false);
+
+    const { 
+        formState, note,
+        isFormValid, noteValid,
+        onInputChange, onResetForm,
+    } = useForm( formData, formValidation );
+
     const onAddNote = ( evt ) => {
         evt.preventDefault();
+        setFormSubmitted(true);
+
+        if(!isFormValid) return;
 
         console.log('agregando nota');
+        console.log(formState);
 
+        // onResetForm();
     }
 
   return (
     <Box sx={{ padding: 4 }}>
-        <form>
+        <form onSubmit={ onAddNote }>
             <Grid item sx={{ mb: 5 }}>
                 <Typography variant="h4">Agregar Nota</Typography>
             </Grid>
@@ -20,6 +43,12 @@ export const AddNote = () => {
                 <TextField
                     variant="filled"
                     label="título nota"
+                    name="note"
+                    autoComplete="off"
+                    value={ note }
+                    onChange={ onInputChange }
+                    error={ !!noteValid  && formSubmitted }
+                    helperText={ noteValid }
                     fullWidth
                 />
             </Grid>
@@ -29,7 +58,6 @@ export const AddNote = () => {
                     variant="contained"
                     type="submit"
                     fullWidth
-                    onClick={ onAddNote }
                 >
                     Agregar Nota
                 </Button>
